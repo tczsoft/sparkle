2ebbf6dfe714b7194d8d416b8674b96875be9446
tczsoft <vinayagam.mhb@gmail.com>
2022-11-26 16:51:14 +0530

    26-11-2022

--- assets/javascript/shadow.js
+++ assets/javascript/shadow.js
@@ -6,13 +6,13 @@ function scroll() {
   var topbar = document.body.scrollTop;
   if (topbar >= 100 || document.documentElement.scrollTop >100) {
 
-    document.getElementsByTagName('header')[0].classList.add("bg-white");
+   // document.getElementsByTagName('header')[0].classList.add("bg-white");
     document.getElementsByTagName('header')[0].classList.add("header-shadow");
     document.getElementById('secondry-toggle-button').classList.add("border")
 
   } 
   else {
-    document.getElementsByTagName('header')[0].classList.remove("bg-white");
+   // document.getElementsByTagName('header')[0].classList.remove("bg-white");
     document.getElementById('secondry-toggle-button').classList.remove("border")
     document.getElementsByTagName('header')[0].classList.remove("header-shadow");
   }
